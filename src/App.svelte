6852c332745c004a9b356e2279f3a40449471906
Amada Mackey <105397288+amada95@users.noreply.github.com>
2022-11-23 21:39:27 -0600

    Complete redo

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,32 @@
-<script>
+<script lang="ts">
   import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
   <div>
-    <h1>The Coalition Journal</h1>
-	<p class="construction">Under Construction</p>
+    <img src="https://simpleicons.org/icons/vercel.svg" class="logo" alt="Vercel Logo" />
+  </div>
+  <h1>The Coalition Journal</h1>
+  <p>
+	Under Construction
+  </p>
+
+  <div class="card">
+    <Counter />
   </div>
 </main>
 
 <style>
-  .construction {
-    color: #888;
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo {
+    filter: drop-shadow(0 0 2em #b4b4b4d9);
+  }
+
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #ffffffff);
   }
 </style>
